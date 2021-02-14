0ba45e71b1c0c73850bb32e7e6014c447ec59d95
drunkmoses <mosesberger@gmail.com>
2021-02-14 20:12:09 +0200

    test upload to github

---
+++ fivetimes.py
@@ -0,0 +1,15 @@
+total = 0
+for num in range(101): # this will count 'num' 101 times while adding it to variable 'total'
+    total = total + num
+    print(total)
+
+i = 0
+while i < 5: # simple while loop that increases every time it runs until it conditions are not met anymore
+    print(i)
+    i = i + 1
+
+for x in range(99, 102): # starts the loop at 99 and ends before 102
+    print(x)
+
+for y in range(104, 200, 4): # the 
+    print(y)
